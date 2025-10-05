2e3c2bbe1f0f008d57a170a3c7654b75f1690ec8
Quinn Keenan <kqfall1@gmail.com>
2025-10-05 18:24:21 -0400

    v1.0: Initial commit of my personal, hand-crafted Java utility/helper functions.The first addition was the CollectionPrinter class.

--- src/main/java/com/github/kqfall1/kqfall1JavaUtils/CollectionPrinter.java
+++ src/main/java/com/github/kqfall1/kqfall1JavaUtils/CollectionPrinter.java
@@ -1,4 +1,30 @@
+// Quinn Keenan, 05/10/2025
+
 package com.github.kqfall1.kqfall1JavaUtils;
 
-public class CollectionPrinter {
+import java.util.List;
+
+public final class CollectionPrinter
+{
+	private CollectionPrinter() {}
+
+	public static void print(Object[] arr, String prompt)
+	{
+		System.out.println(String.format("%s:", prompt));
+
+		for (Object o : arr)
+		{
+			System.out.println(String.format("\t%s", o));
+		}
+	}
+
+	public static void print(List<?> list, String prompt)
+	{
+		System.out.println(String.format("%s:", prompt));
+
+		for (Object o : list)
+		{
+			System.out.println(String.format("\t%s", o));
+		}
+	}
 }
