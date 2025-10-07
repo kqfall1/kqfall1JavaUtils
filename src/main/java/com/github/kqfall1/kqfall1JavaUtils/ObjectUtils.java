6b4bdef0822504d5d1a992d2188ffa37ca7c624a
Quinn Keenan <kqfall1@gmail.com>
2025-10-06 22:16:53 -0400

    v1.26: Added ObjectUtil and ObjectUtil.validateArgument.

---
+++ src/main/java/com/github/kqfall1/kqfall1JavaUtils/ObjectUtils.java
@@ -0,0 +1,18 @@
+// Quinn Keenan, 06/10/2025
+
+package com.github.kqfall1.kqfall1JavaUtils;
+
+public final class ObjectUtils
+{
+	public static void validateArgument(double arg, String argName, double lowerBound, double upperBound)
+	{
+		if (arg < lowerBound || arg > upperBound)
+		{
+			throw new IllegalArgumentException(
+				String.format("\n%s\n needs to remain between %.2f and %.2f inclusive. %.2f is invalid.",
+					argName, lowerBound, upperBound, arg
+				)
+			);
+		}
+	}
+}
